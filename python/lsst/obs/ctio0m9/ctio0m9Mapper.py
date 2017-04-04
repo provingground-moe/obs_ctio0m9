e99a95eefbeb701cf71132a76006ac407489b56f
Robert Lupton the Good <rhl@astro.princeton.edu>
2017-04-04 13:14:27 -0500

    Added a "clear" filter

--- python/lsst/obs/ctio0m9/ctio0m9Mapper.py
+++ python/lsst/obs/ctio0m9/ctio0m9Mapper.py
@@ -59,7 +59,7 @@ class Ctio0m9Mapper(CameraMapper):
 
         CameraMapper.__init__(self, policy, policyFile.getRepositoryPath(), **kwargs)
 
-        afwImageUtils.defineFilter('NONE', 0.0, alias=['no_filter', "OPEN"])
+        afwImageUtils.defineFilter('NONE', 0.0, alias=['no_filter', "OPEN", "clear"])
         afwImageUtils.defineFilter('Ronchi', 0.0, alias=[])
 
     def _makeCamera(self, policy, repositoryDir):

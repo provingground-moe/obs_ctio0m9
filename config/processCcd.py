56ef4045d3a9d751e87bd18e3f96276c579e4998
Merlin Fisher-Levine <merlin.fisherlevine@gmail.com>
2017-05-19 17:15:52 -0500

    Add processCcd config for Gaia catalog

--- config/processCcd.py
+++ config/processCcd.py
@@ -1,26 +1,35 @@
+from lsst.meas.algorithms import LoadIndexedReferenceObjectsTask
+
 config.isr.doFlat=False
 config.isr.doLinearize=False
 config.isr.doDefect=False
 
 config.charImage.repair.cosmicray.nCrPixelMax=100000
 
+config.calibrate.photoRefObjLoader.retarget(LoadIndexedReferenceObjectsTask)
+config.calibrate.photoRefObjLoader.ref_dataset_name = "gaia_DR1_v1"
+config.calibrate.astromRefObjLoader.retarget(LoadIndexedReferenceObjectsTask)
+config.calibrate.astromRefObjLoader.ref_dataset_name = "gaia_DR1_v1"
+config.charImage.refObjLoader.retarget(LoadIndexedReferenceObjectsTask)
+config.charImage.refObjLoader.ref_dataset_name = "gaia_DR1_v1"
+
 for refObjLoader in (config.calibrate.astromRefObjLoader,
                      config.calibrate.photoRefObjLoader,
                      config.charImage.refObjLoader):
-    refObjLoader.filterMap = {'RONCHI200+z': 'z',
-						      'NONE+SEMROCK': 'g',
-                              'NONE+RONCHI200': 'g',
-                              'RONCHI200+SEMROCK': 'g',
-                              'NONE+NONE': 'g',
-                              'NONE+g': 'g',
-                              'NONE+r': 'r',
-                              'NONE+i': 'i',
-                              'NONE+z': 'z',
-                              'RONCHI200+g': 'g',
-                              'FGB37+RONCHI200': 'g',
-                              'NONE+RONCHI400': 'g',
-                              'FGC715S+RONCHI400': 'g',
-                              'FGC715S+RONCHI200': 'g'}
+   refObjLoader.filterMap = {'RONCHI200+z': 'phot_g_mean_mag',
+                              'NONE+SEMROCK': 'phot_g_mean_mag',
+                              'NONE+RONCHI200': 'phot_g_mean_mag',
+                              'RONCHI200+SEMROCK': 'phot_g_mean_mag',
+                              'NONE+NONE': 'phot_g_mean_mag',
+                              'NONE+g': 'phot_g_mean_mag',
+                              'NONE+r': 'phot_g_mean_mag',
+                              'NONE+i': 'phot_g_mean_mag',
+                              'NONE+z': 'phot_g_mean_mag',
+                              'RONCHI200+g': 'phot_g_mean_mag',
+                              'FGB37+RONCHI200': 'phot_g_mean_mag',
+                              'NONE+RONCHI400': 'phot_g_mean_mag',
+                              'FGC715S+RONCHI400': 'phot_g_mean_mag',
+                              'FGC715S+RONCHI200': 'phot_g_mean_mag'}
 
 # config.calibrate.astrometry.wcsFitter.order = 3
 # config.calibrate.astrometry.matcher.maxMatchDistArcSec = 5

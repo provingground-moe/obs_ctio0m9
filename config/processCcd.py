530fe6eadf520444ef1205dc560823954b1cf3de
Merlin Fisher-Levine <merlin.fisherlevine@gmail.com>
2017-06-06 17:38:08 -0500

    Overhaul processCcd config override
    Many changes were necessary to get all the matching working correctly
    Consider this commit a total replacement for previous configs.
    Now contains comments explaining each config choice made too

--- config/processCcd.py
+++ config/processCcd.py
@@ -1,11 +1,13 @@
 from lsst.meas.algorithms import LoadIndexedReferenceObjectsTask
+from lsst.meas.astrom import MatchPessimisticBTask
 
-config.isr.doFlat=False
+config.isr.doFlat=False # TODO: change for release/when we have flats
 config.isr.doLinearize=False
-config.isr.doDefect=False
+config.isr.doDefect=False # TODO: make defect list and enable
 
 config.charImage.repair.cosmicray.nCrPixelMax=100000
 
+# Retarget all matchers to use Gaia
 config.calibrate.photoRefObjLoader.retarget(LoadIndexedReferenceObjectsTask)
 config.calibrate.photoRefObjLoader.ref_dataset_name = "gaia_DR1_v1"
 config.calibrate.astromRefObjLoader.retarget(LoadIndexedReferenceObjectsTask)
@@ -13,10 +15,11 @@ config.calibrate.astromRefObjLoader.ref_dataset_name = "gaia_DR1_v1"
 config.charImage.refObjLoader.retarget(LoadIndexedReferenceObjectsTask)
 config.charImage.refObjLoader.ref_dataset_name = "gaia_DR1_v1"
 
+# Create a filterMap for all known filters to the one Gaia "filter"
 for refObjLoader in (config.calibrate.astromRefObjLoader,
                      config.calibrate.photoRefObjLoader,
                      config.charImage.refObjLoader):
-   refObjLoader.filterMap = {'RONCHI200+z': 'phot_g_mean_mag',
+    refObjLoader.filterMap = {'RONCHI200+z': 'phot_g_mean_mag',
                               'NONE+SEMROCK': 'phot_g_mean_mag',
                               'NONE+RONCHI200': 'phot_g_mean_mag',
                               'RONCHI200+SEMROCK': 'phot_g_mean_mag',
@@ -31,5 +34,41 @@ for refObjLoader in (config.calibrate.astromRefObjLoader,
                               'FGC715S+RONCHI400': 'phot_g_mean_mag',
                               'FGC715S+RONCHI200': 'phot_g_mean_mag'}
 
-# config.calibrate.astrometry.wcsFitter.order = 3
-# config.calibrate.astrometry.matcher.maxMatchDistArcSec = 5
+# Switch to using psfex if possible
+try:
+    import lsst.meas.extensions.psfex.psfexPsfDeterminer
+    config.charImage.measurePsf.psfDeterminer["psfex"].spatialOrder = 1
+    config.charImage.measurePsf.psfDeterminer["psfex"].psfexBasis = 'PIXEL_AUTO'
+    config.charImage.measurePsf.psfDeterminer.name = "psfex"
+except ImportError as e:
+    print("WARNING: Unable to use psfex: %s" % e)
+
+# retarget both the astrometric matchers
+config.calibrate.astrometry.matcher.retarget(MatchPessimisticBTask)
+config.calibrate.photoCal.matcher.retarget(MatchPessimisticBTask)
+
+# improve threshold values for short exposures on a small telescope
+config.charImage.detection.includeThresholdMultiplier=5.0 #default=10.0
+config.charImage.measurePsf.starSelector['objectSize'].fluxMin=1000.0 # default=12500.0
+config.calibrate.astrometry.matcher.sourceSelector['matcher'].minSnr=4.0 # default=40.0
+config.calibrate.photoCal.matcher.sourceSelector['matcher'].minSnr=4.0 # default=40.0
+
+# probably don't need these, was to check that no sources were being thrown away due to these
+# TODO: remove these options and rerun all to check if everything still works.
+#       if does, remove these lines.
+config.calibrate.astrometry.matcher.sourceSelector['matcher'].badFlags=[]
+config.calibrate.astrometry.matcher.sourceSelector['astrometry'].badFlags=[]
+
+# Carefully tuned for matchPessimisticB; less than 2000 is too little for some visits
+# but more than 2000 has so many sources to match against that it runs _very_ slowly
+# for current dataset, an offset of 2000pix matches all direct visits
+config.calibrate.astrometry.matcher.maxOffsetPix=2000
+config.calibrate.photoCal.matcher.maxOffsetPix=2000
+config.calibrate.astrometry.wcsFitter.order=2 # prevent overfitting as we don't have many stars
+
+# we don't need these, and sometimes the correction can't be measured - this prevents failure
+config.calibrate.doApCorr=False 
+config.charImage.doApCorr=False
+
+# try to reduce spurious detections around the bright sources
+config.calibrate.detection.doTempLocalBackground=True
